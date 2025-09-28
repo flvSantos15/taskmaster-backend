07e20ce80891c6819a9d2d90341412fcd1826678
flvSantos15 <flvsantos300@gmail.com>
2025-09-27 21:33:48 -0300

    feat: add routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import cors from "cors"
-import express from "express"
-import helmet from "helmet"
-import morgan from "morgan"
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import authRoutes from './routes/auth.routes';
+import taskRoutes from './routes/tasks.routes';
 
 const app = express()
 
@@ -12,10 +14,13 @@ app.use(morgan('combined'))
 app.use(express.json())
 
 // Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 // Health check
 app.get('/health', (req, res) => {
   res.json({ status: 'OK', timestamp: new Date().toISOString() })
 })
 
-export { app }
+export { app };
+
